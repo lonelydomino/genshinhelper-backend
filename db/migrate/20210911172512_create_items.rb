719a11c6e6b5c8ab77dace4bf610ea301d02f7a5
Milo <zeroeclipse@live.com>
2021-11-22 15:40:03 -0500

    Updating items table.

--- db/migrate/20210911172512_create_items.rb
+++ db/migrate/20210911172512_create_items.rb
@@ -2,11 +2,10 @@ class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
       t.string :name
-      t.string :day1
-      t.string :day2
-      t.string :day3
+      t.text :days, array: true, default: []
       t.integer :domain_id
       t.timestamps
+      t.string :rarity
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -59,12 +59,11 @@ ActiveRecord::Schema.define(version: 2021_09_11_175555) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
-    t.string "day1"
-    t.string "day2"
-    t.string "day3"
+    t.text "days", default: [], array: true
     t.integer "domain_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "rarity"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
